8d92828bfd71da4d4575862df4aab6effdb8c8d7
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-14 14:01:45 -0500

    useCounter - Custom Hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
-// import { HookApp } from './HookApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 
 ReactDOM.render(
-  <CounterApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 );
